77f487d4ef4b2586e67f78d055d6e197dd9d9833
RahulNeupane <nrahul503@gmail.com>
2022-11-23 00:02:43 +0545

    college practise

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <h1>This is welcome page hai</h1>
+        <button class="btn btn-success">Save</button>
+        <button class="btn btn-primary">Confirm</button>
+        <button class="btn btn-danger">Delete</button>
+    </div>
+@endsection
